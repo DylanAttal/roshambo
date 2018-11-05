80b138c89c9cb3675d90c91f9b8549d52d53f3ae
Dylan Attal <attal.dylan@gmail.com>
2018-11-05 08:10:45 -0500

    Added button-glows, responsive media queries. Disabled unused buttons.

--- public/main.js
+++ public/main.js
@@ -8,22 +8,52 @@ const declareWinner = () => {
   let winDeclaration = document.querySelector('.results-section h2')
 
   if (playerChoice === 'rock' && computerChoice === 'rock') {
+    document.querySelector('.player-section .rock').classList.add('green-glow')
+    document.querySelector('.computer-section .rock').classList.add('red-glow')
     winDeclaration.textContent = "It's a tie!"
   } else if (playerChoice === 'rock' && computerChoice === 'paper') {
+    document.querySelector('.player-section .rock').classList.add('green-glow')
+    document.querySelector('.computer-section .paper').classList.add('red-glow')
     winDeclaration.textContent = 'Computer wins!'
   } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+    document.querySelector('.player-section .rock').classList.add('green-glow')
+    document
+      .querySelector('.computer-section .scissors')
+      .classList.add('red-glow')
     winDeclaration.textContent = 'Player wins!'
   } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+    document.querySelector('.player-section .paper').classList.add('green-glow')
+    document.querySelector('.computer-section .rock').classList.add('red-glow')
     winDeclaration.textContent = 'Player wins!'
   } else if (playerChoice === 'paper' && computerChoice === 'paper') {
+    document.querySelector('.player-section .paper').classList.add('green-glow')
+    document.querySelector('.computer-section .paper').classList.add('red-glow')
     winDeclaration.textContent = "It's a tie!"
   } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+    document.querySelector('.player-section .paper').classList.add('green-glow')
+    document
+      .querySelector('.computer-section .scissors')
+      .classList.add('red-glow')
     winDeclaration.textContent = 'Computer wins!'
   } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
+    document
+      .querySelector('.player-section .scissors')
+      .classList.add('green-glow')
+    document.querySelector('.computer-section .rock').classList.add('red-glow')
     winDeclaration.textContent = 'Computer wins!'
   } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+    document
+      .querySelector('.player-section .scissors')
+      .classList.add('green-glow')
+    document.querySelector('.computer-section .paper').classList.add('red-glow')
     winDeclaration.textContent = 'Player wins!'
   } else if (playerChoice === 'scissors' && computerChoice === 'scissors') {
+    document
+      .querySelector('.player-section .scissors')
+      .classList.add('green-glow')
+    document
+      .querySelector('.computer-section .scissors')
+      .classList.add('red-glow')
     winDeclaration.textContent = "It's a tie!"
   } else {
     winDeclaration.textContent = 'Something went wrong!'
@@ -32,16 +62,22 @@ const declareWinner = () => {
 
 const playerClickRock = () => {
   playerChoice = 'rock'
+  document.querySelector('.player-section .paper').disabled = true
+  document.querySelector('.player-section .scissors').disabled = true
   declareWinner()
 }
 
 const playerClickPaper = () => {
   playerChoice = 'paper'
+  document.querySelector('.player-section .rock').disabled = true
+  document.querySelector('.player-section .scissors').disabled = true
   declareWinner()
 }
 
 const playerClickScissors = () => {
   playerChoice = 'scissors'
+  document.querySelector('.player-section .rock').disabled = true
+  document.querySelector('.player-section .paper').disabled = true
   declareWinner()
 }
 
